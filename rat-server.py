7237e43dda2bf5b5bf3e2df070f242d8a7ac54c4
root <root@localhost.localdomain>
2018-02-05 15:22:03 -0500

    server V1

--- rat-server.py
+++ rat-server.py
@@ -1,1 +1,28 @@
-test
+#################################################
+#Auteur: Vikrant SINGH				#
+#Programme: Socket-serveur			#
+#						#
+#################################################
+
+
+#################################################
+#####################Imports#####################
+
+import socket
+
+
+#################################################
+
+
+hote = ' '
+port = 5050
+
+#SOCK_STREAM pour effectuer une connexion TCP et AF_INET pour l'utilisation adresse IPV4
+server_side = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#On configure notre serveur pour qu'il écoute sur un port
+server_side.bind((hote, port))
+
+#Nombre de connexion  simultanée
+server_side.listen(5)
+
